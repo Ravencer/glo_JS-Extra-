d5388d190ea717da0fdb388d284bcb27287c9ff5
Ravencer <ravencer880@gmail.com>
2021-01-30 18:45:05 +0700

    Выполнил второе усложненное задание

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+let num = 266219;
+let multiNum = 1;
+while (num > 0){
+  multiNum *= num % 10;
+  num = parseInt(num / 10);
+}
+console.log(multiNum);
+multiNum **= 3;
+multiNum = multiNum.toString();
+console.log(multiNum[0], multiNum[1]);
