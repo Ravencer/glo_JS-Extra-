3ec15b9a204af27adea4147cbbf0802887703a3f
Ravencer <ravencer880@gmail.com>
2021-02-01 22:40:29 +0700

    Выполнил третье усложненное задание

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
-let num = 266219;
-let multiNum = 1;
-while (num > 0){
-  multiNum *= num % 10;
-  num = parseInt(num / 10);
+let lang = prompt('Введите предпочитаемый язык?');
+let day = new Date();
+let daysRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let daysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log('Через if');
+if(lang === "ru"){
+  for (let i = 0; i < 7; i++){
+    console.log(daysRu[i]);
+  }
 }
-console.log(multiNum);
-multiNum **= 3;
-multiNum = multiNum.toString();
-console.log(multiNum[0], multiNum[1]);
+else if (lang === "en"){
+  for (let i = 0; i < 7; i++){
+    console.log(daysEn[i]);
+  }
+}
+console.log('Через switch');
+switch(lang){
+  case "ru":
+    for (let i = 0; i < 7; i++){
+      console.log(daysRu[i]);
+    }
+    break;
+  case "en":
+    for (let i = 0; i < 7; i++){
+      console.log(daysEn[i]);
+    }
+    break;
+}
+let daysArr = [
+  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+];
+let result = lang === "ru" ? 0 : 1;
+console.log('Через многомерные массивы');
+for (let i = 0; i < 7; i++){
+  console.log(daysArr[result][i]);
+}
+
+
+let namePerson = prompt('Введите имя: ');
+let resultName = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
+console.log(resultName);
