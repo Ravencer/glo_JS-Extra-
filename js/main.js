ba70a9e0b9e403bc4e4f108d69e635c3380a10de
Ravencer <ravencer880@gmail.com>
2021-02-02 14:21:49 +0700

    Выполнил четвертое усложненное задание

--- js/main.js
+++ js/main.js
@@ -1,42 +1,19 @@
-let lang = prompt('Введите предпочитаемый язык?');
-let day = new Date();
-let daysRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let daysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-console.log('Через if');
-if(lang === "ru"){
-  for (let i = 0; i < 7; i++){
-    console.log(daysRu[i]);
+'use strict';
+
+function stringExtension(args){
+  let trimmed = ' ';
+  if(typeof(args) !== "string"){
+    console.log("Вы ввели не строку!");
+    console.log(typeof(args));
   }
-}
-else if (lang === "en"){
-  for (let i = 0; i < 7; i++){
-    console.log(daysEn[i]);
+  else {
+    trimmed = args.trim();
+    if(trimmed.length > 30){
+      trimmed = trimmed.slice(0, 30);
+      trimmed += "...";
+    }
   }
-}
-console.log('Через switch');
-switch(lang){
-  case "ru":
-    for (let i = 0; i < 7; i++){
-      console.log(daysRu[i]);
-    }
-    break;
-  case "en":
-    for (let i = 0; i < 7; i++){
-      console.log(daysEn[i]);
-    }
-    break;
-}
-let daysArr = [
-  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-];
-let result = lang === "ru" ? 0 : 1;
-console.log('Через многомерные массивы');
-for (let i = 0; i < 7; i++){
-  console.log(daysArr[result][i]);
+  return trimmed;
 }
 
-
-let namePerson = prompt('Введите имя: ');
-let resultName = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
-console.log(resultName);
+console.log(stringExtension("   Приветвфывдолфывдлфоывлдфыовфдлывофдлывофдывлофдылвофывдлфоывдлфывофдлывофдылвофдылвофыдвлофывдлфоывдлфыво   "));
