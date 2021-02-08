fef60721e3a7158a56bfe3e3d577538b119d7090
Ravencer <ravencer880@gmail.com>
2021-02-08 12:46:01 +0700

    Выполнил седьмое усложненное задание

--- js/main.js
+++ js/main.js
@@ -1,50 +1,16 @@
 'use strict';
 
-let arr = ["23123", "4312312", "12312312", "5555", "4444", "2222", "9312"];
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let today = new Date();
 
-function checkNum(num){
-  num = parseInt(num);
-  let digit = 0;
-  while(num > 0){
-    digit = num % 10;
-    num = parseInt(num / 10);
+for(let i = 0; i < week.length; i++){
+  if(week[i] !== 'Суббота' && week[i] !== 'Воскресенье' && today.getDay() !== i){
+    document.write('<p class="week-day">' + week[i] + '</p>');
   }
-  if(digit === 2 || digit === 4){
-    return true;
+  else if(today.getDay() === i){
+    document.write('<p class="week-day" style="font-weight: bold;">' + week[i] + '</p>');
   }
   else{
-    return false;
+    document.write('<p class="week-day" style="font-style: italic;">' + week[i] + '</p>');
   }
-}
-
-function writeNum (arr){
-  for(let i = 0; i < arr.length; i++){
-    if(checkNum(arr[i])){
-      console.log(arr[i]);
-    }
-    
-  }
-}
-writeNum(arr);
-
-let n = 100;
-for (let i = 2; i < n+1; i++) {
-    let flag = 1;
-    if (i > 2 && i % 2 !== 0)
-    {
-        for (let j = 3; j*j <= i ; j=j+2)
-        {
-            if (i % j===0)
-            {
-              flag = 0;
-              break;
-            }
-        }
-    }
-    else if (i !== 2) {
-      flag = 0;
-    }
-    if (flag === 1) {
-      console.log(i + " делится на 1 и " + i);
-    }
 }
