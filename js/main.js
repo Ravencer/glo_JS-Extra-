a75e1e249aa07f329459090191909226f71015bc
Ravencer <ravencer880@gmail.com>
2021-02-20 15:45:10 +0700

    Выполнил четырнадцатое обязательное задание

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 'use strict';
 
-let buttonChange = document.querySelector('#change');
-let textChange = document.querySelector('#color');
 
+function DomElement(selector, height, width, bg, fontsize){
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.fontsize = fontsize;
+  this.bg = bg;
+}
 
-buttonChange.addEventListener('click', () =>{
-  let randomHex = Math.floor(Math.random()*16777215).toString(16);
-  textChange.textContent = '#' + randomHex;
-  document.body.style.backgroundColor = '#' + randomHex;
-});
+DomElement.prototype.createElement = function(){
+  if(this.selector.slice(0,1) === '.'){
+    let newElem = document.createElement('div');
+    newElem.className = this.selector.slice(1);
+    newElem.style.height = this.height + 'px';
+    newElem.style.width = this.width + 'px';
+    newElem.style.fontSize = this.fontsize + 'px';
+    newElem.style.background = this.bg;
+    newElem.textContent = 'Hello World!';
+    return newElem;
+  }
+  else if(this.selector.slice(0,1) === '#'){
+    let newElem = document.createElement('p');
+    newElem.className = this.selector.slice(1);
+    newElem.style.height = this.height + 'px';
+    newElem.style.width = this.width + 'px';
+    newElem.style.fontSize = this.fontsize + 'px';
+    newElem.style.background = this.bg;
+    newElem.textContent = 'Hello World!';
+    return newElem;
+  }
+  else{
+    alert('Вы ввели неверную строку!');
+    return null;
+  }
+};
 
+let domElem = new DomElement(prompt('Введите строку selector'), 50, 100, 'green', 14);
+document.body.append(domElem.createElement());
